a97ba7a6b73e33cb98c3e8a4abd66b0aaaba5851
deniska <denis.yesakov@gmail.com>
2019-06-12 23:05:02 +0200

    initial commit 2

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,3 @@
+test('Hello Simple parser for redis url', () => {
+    expect('Hello Simple parser for redis url').toBe('Hello Simple parser for redis url');
+});
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+namespace ParseRedisUrl {
+    const redisDefaultPort = 6379;
+    const sentinelDefaultPort = 26379;
+    export function parseRedisUrl(url: string, sentinel: boolean = false, separator: string = ',; '): string[] {
+        const result = <string[]>[];
+
+        return result;
+    }
+}
